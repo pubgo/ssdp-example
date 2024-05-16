2100979949ab9222d0752a1d5d5ade2dcbdb3bee
barry <kooksee@163.com>
2024-05-16 19:18:27 +0800

    fix: 2024-05-16 19:18:27

--- peerdiscovery/main.go
+++ peerdiscovery/main.go
@@ -10,7 +10,9 @@ import (
 
 func main() {
 	discoveries, _ := peerdiscovery.Discover(peerdiscovery.Settings{
-		Limit: 1, Delay: time.Second * 20})
+		Limit: 1, Delay: time.Second * 20,
+		AllowSelf: true,
+	})
 	for _, d := range discoveries {
 		fmt.Printf("discovered '%s'\n", d.Address)
 	}
